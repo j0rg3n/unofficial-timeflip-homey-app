e41514427c81c6ec771f334c4edb369513ffc299
Jørgen <jorgen@fabeljet.com>
2026-04-13 22:56:59 +0200

    Add Homey driver and device integration
    - .homeycompose/app.json: BLE permission, device definition, flow cards,
      capabilities, insights logs
    - drivers/timeflip/driver.js: pairing flow with BLE scan
    - drivers/timeflip/device.js: thin adapter to TimeFlipController
    - app.js: flow card implementations (triggers, conditions, actions)

---
+++ com.fabeljet.unofficial-timeflip2-driver/app.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const Homey = require('homey');
+
+class TimeFlipApp extends Homey.App {
+  async onInit() {
+    this.log('TimeFlipApp has been initialized');
+
+    const facetChangedTrigger = this.homey.flow.getTriggerCard('facet_changed');
+    const doubleTapTrigger = this.homey.flow.getTriggerCard('double_tap');
+
+    this.on('trigger:facet_changed', ({ device, tokens }) => {
+      facetChangedTrigger.trigger(device, tokens).catch(this.error);
+    });
+
+    this.on('trigger:double_tap', ({ device, tokens }) => {
+      doubleTapTrigger.trigger(device, tokens).catch(this.error);
+    });
+
+    const facetIsCondition = this.homey.flow.getConditionCard('facet_is');
+    facetIsCondition.registerRunListener(async (args, state) => {
+      return state.device.getCapabilityValue('timeflip:facet') === parseInt(args.facet);
+    });
+
+    const isPausedCondition = this.homey.flow.getConditionCard('is_paused');
+    isPausedCondition.registerRunListener(async (args, state) => {
+      return !state.device.getCapabilityValue('onoff');
+    });
+
+    const isLockedCondition = this.homey.flow.getConditionCard('is_locked');
+    isLockedCondition.registerRunListener(async (args, state) => {
+      return state.device.getCapabilityValue('locked');
+    });
+
+    const setPauseAction = this.homey.flow.getActionCard('set_pause');
+    setPauseAction.registerRunListener(async (args, device) => {
+      const controller = device._controller;
+      if (controller) {
+        await controller.setPause(args.value === 'on');
+      }
+    });
+
+    const setLockAction = this.homey.flow.getActionCard('set_lock');
+    setLockAction.registerRunListener(async (args, device) => {
+      const controller = device._controller;
+      if (controller) {
+        await controller.setLock(args.value === 'on');
+      }
+    });
+
+    const setAutoPauseAction = this.homey.flow.getActionCard('set_auto_pause');
+    setAutoPauseAction.registerRunListener(async (args, device) => {
+      const controller = device._controller;
+      if (controller) {
+        await controller.setAutoPause(args.minutes);
+      }
+    });
+
+    const syncTimeAction = this.homey.flow.getActionCard('sync_time');
+    syncTimeAction.registerRunListener(async (args, device) => {
+      const controller = device._controller;
+      if (controller) {
+        await controller.syncTime();
+      }
+    });
+  }
+}
+
+module.exports = TimeFlipApp;
---
+++ com.fabeljet.unofficial-timeflip2-driver/drivers/timeflip/driver.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const Homey = require('homey');
+
+class TimeFlipDriver extends Homey.Driver {
+  async onInit() {
+    this.log('TimeFlipDriver has been initialized');
+  }
+
+  async onPairListDevices() {
+    const devices = [];
+
+    try {
+      const peripherals = await this.homey.ble.find();
+
+      for (const peripheral of Object.values(peripherals)) {
+        const advertisement = peripheral.advertisement;
+        const localName = advertisement.localName || '';
+        
+        if (localName.includes('TimeFlip')) {
+          devices.push({
+            name: peripheral.localName || 'TimeFlip 2',
+            data: {
+              id: peripheral.id,
+            },
+          });
+        }
+      }
+    } catch (err) {
+      this.log('Error scanning for devices:', err.message);
+    }
+
+    return devices;
+  }
+}
+
+module.exports = TimeFlipDriver;
